b364ecb931f331a30f481879bf047bb4197616b8
Sebastian <sebastiano139@gmail.com>
2020-12-05 22:24:04 +0100

    Added manufacurer and admin view

--- sneaker-chain/src/components/Admin/Admin.tsx
+++ sneaker-chain/src/components/Admin/Admin.tsx
@@ -1,9 +1,82 @@
-import React, { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { makeStyles } from "@material-ui/core";
+import { Container, Grid, Button } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
+
+interface IFormInput {
+  street: String;
+  city: String;
+  postalCode: String;
+  state: String;
+  country: String;
+}
 
 export function Admin(): ReactElement {
+  const [address, setAddress] = useState(0);
+
+  const {register, handleSubmit} = useForm();
+
+  const useStyles = makeStyles((theme) => ({
+    header: {
+      display: "flex",
+      justifyContent: "center",
+      marginTop: 10,
+      marginBottom: 20,
+      fontSize: 25
+    },
+
+    formControl: {
+      width: '100%',
+      marginTop: 10
+    }
+
+  }));
+
+  const onSubmit = (data: IFormInput) => console.log(data);
+  const classes = useStyles();
+
   return (
     <div>
-      <span>ADMIN</span>
+      <Container>
+        <Grid>
+          <h1 className={classes.header}>
+            Add new Manufacturer
+          </h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+              <label htmlFor="streetLabel" className="ml-2">
+                Street
+              </label>
+              <input name="street" ref={register({required: true})} type="text" id="streetId" className="form-control mb-2"/>
+
+              <label htmlFor="cityLabel" className="ml-2">
+                City
+              </label>
+              <input name="city" ref={register} type="text" className="form-control mb-2"/>
+
+              <label htmlFor="postalCodeLabel" className="ml-2">
+                Postal Code
+              </label>
+              <input name="postalCode" ref={register} type="text" id="streetId" className="form-control mb-2"/>
+
+              <label htmlFor="stateLabel" className="ml-2">
+                State
+              </label>
+              <input name="state" ref={register} type="text" id="streetId" className="form-control mb-2"/>
+
+              <label htmlFor="countryLabel" className="ml-2">
+                Country
+              </label>
+              <input name="country" ref={register} type="text" id="streetId" className="form-control mb-2"/>
+
+            <div className="text-center mt-4">
+              <Button type="submit" color="primary" variant="outlined">
+                Submit
+              </Button>
+            </div>
+          </form>
+
+        </Grid>
+      </Container>
     </div>
   );
 }
--- sneaker-chain/src/components/Manufacturer/Manufacturer.tsx
+++ sneaker-chain/src/components/Manufacturer/Manufacturer.tsx
@@ -1,9 +1,68 @@
-import React, { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { makeStyles } from "@material-ui/core";
+import { Container, Grid, Button } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
+
+interface IFormInput {
+  name: String;
+  modelId: String;
+  size: number;
+}
 
 export function Manufacturer(): ReactElement {
+
+  const {register, handleSubmit} = useForm();
+
+  const useStyles = makeStyles((theme) => ({
+    header: {
+      display: "flex",
+      justifyContent: "center",
+      marginTop: 10,
+      marginBottom: 20,
+      fontSize: 25
+    },
+
+    formControl: {
+      width: '100%',
+      marginTop: 10
+    }
+  }));
+
+  const classes = useStyles();
+  const onSubmit = (data: IFormInput) => console.log(data);
+
   return (
     <div>
-      <span>MANUFACTURER</span>
+     <Container>
+        <Grid>
+          <h1 className={classes.header}>
+            Add new Shoes
+          </h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+              <label htmlFor="nameLabel" className="ml-2">
+                Name
+              </label>
+              <input name="name" ref={register({required: true})} type="text" id="streetId" className="form-control mb-2"/>
+
+              <label htmlFor="modelId" className="ml-2">
+                Model ID
+              </label>
+              <input name="modelId" ref={register} type="text" className="form-control mb-2"/>
+
+              <label htmlFor="sizeLabel" className="ml-2">
+                Size
+              </label>
+              <input name="size" ref={register} type="text" id="streetId" className="form-control mb-2"/>
+
+            <div className="text-center mt-4">
+              <Button type="submit" color="primary" variant="outlined">
+                Submit
+              </Button>
+            </div>
+          </form>
+
+        </Grid>
+      </Container>
     </div>
   );
 }
